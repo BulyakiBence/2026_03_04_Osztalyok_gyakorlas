f6dadff9d2778b5dbbb95fc1bb0418bab52be9a7
BulyakiBence <bulyaki.bence.benedikt@diak.szbi-pg.hu>
2026-03-04 09:52:32 +0100

    3.ora

--- auto.py
+++ auto.py
@@ -1,10 +1,11 @@
 class Auto:
-    def __init__(self, marka_, tipus_, gyartasi_ev_):
+    def __init__(self, marka_, tipus_, gyartasi_ev_, fogyasztas_, uzemanyag_ = 50):
         self.marka= marka_
         self.tipus = tipus_
         self.gyartasi_ev = gyartasi_ev_
         self.sebesseg = 0
-
+        self.fogyasztas = fogyasztas_
+        self.uzemanyag = uzemanyag_
     def __str__(self):
         return f"{self.marka} {self.tipus} ({self.gyartasi_ev}), sebesség: {self.sebesseg} km/h "
     
@@ -17,3 +18,8 @@ class Auto:
         self.sebesseg -= ertek
         if self.sebesseg < 0:
             self.sebesseg = 0
+
+    def tankol(self, mennyiseg):
+        self.uzemanyag += mennyiseg
+        if self.uzemanyag > 50:
+            self.uzemanyag = 50
